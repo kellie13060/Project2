7495fb926359581fc3ffd883c4b0a0ebd6c6e515
Stephen Green <stephengreen1028@gmail.com>
2020-02-11 21:40:01 -0700

    changed dbUser to usersDB on passport.js

--- config/passport.js
+++ config/passport.js
@@ -15,26 +15,28 @@ passport.use(
     },
     function(email, password, done) {
       // When a user tries to sign in this code runs
-      db.User.findOne({
-        where: {
-          email: email
-        }
-      }).then(function(dbUser) {
-        // If there's no user with the given email
-        if (!dbUser) {
-          return done(null, false, {
-            message: "Incorrect email."
-          });
-        }
-        // If there is a user with the given email, but the password the user gives us is incorrect
-        else if (!dbUser.validPassword(password)) {
-          return done(null, false, {
-            message: "Incorrect password."
-          });
-        }
-        // If none of the above, return the user
-        return done(null, dbUser);
-      });
+      db.usersDB
+        .findOne({
+          where: {
+            email: email
+          }
+        })
+        .then(function(usersDB) {
+          // If there's no user with the given email
+          if (!usersDB) {
+            return done(null, false, {
+              message: "Incorrect email."
+            });
+          }
+          // If there is a user with the given email, but the password the user gives us is incorrect
+          else if (!usersDB.validPassword(password)) {
+            return done(null, false, {
+              message: "Incorrect password."
+            });
+          }
+          // If none of the above, return the user
+          return done(null, usersDB);
+        });
     }
   )
 );
