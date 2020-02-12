be09ee13f56a44525fcdeb5501a9b45b1527349b
Pilot <trevorjburni@gmail.com>
2020-02-11 20:25:04 -0700

    adjustments made to api routes file

---
+++ public/js/game.js
@@ -0,0 +1,20 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $("#start-game").on("click", function(event) {
+
+        console.log("Button clicked");
+
+        // hide button
+        $("#start-game").hide();
+
+        $.ajax("/api/getTriviaQuestions", {
+            type: "GET",
+
+        }).then(
+            function(res) {
+                console.log("grabbed data");
+                console.log(res);
+            }
+        );
+    });
+});
