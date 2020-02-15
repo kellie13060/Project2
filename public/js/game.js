efb887eb96d349ba0fb4b17600465c555917a32d
Pilot <trevorjburni@gmail.com>
2020-02-14 21:42:52 -0700

    The game page is alivegit checkout master

--- public/js/game.js
+++ public/js/game.js
@@ -1,13 +1,7 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-
-var seconds = 60;
-var intervalId;
+// set correct variable
 var correct = 0;
-var timeOut;
-
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
-
-
   $("#start-game").on("click", function (event) {
     console.log("Button clicked");
 
@@ -15,10 +9,12 @@ $(function () {
     $("#start-game").hide();
     $(".game").show();
 
+    // Ajax call to the backend to get the trivia questions
     $.ajax("/api/getTriviaQuestions", {
       type: "GET"
     }).then(function (res) {
       console.log(res);
+
       // loop through the response results and assign variables
       for (var i = 0; i < res.results.length; i++) {
         var difficulty = res.results[i].difficulty;
@@ -39,27 +35,32 @@ $(function () {
         var btnDiv = $("<div>");
         btnDiv.attr("class", "container");
         btnDiv.css("padding", "20px");
+
         // True button
         var trueAnswerBtn = $("<button>");
         trueAnswerBtn.css("margin", "0px 15px 0px 15px");
-        trueAnswerBtn.attr("class", "button is-medium answer");
+        trueAnswerBtn.attr("class", "button is-medium answer not-selected");
         trueAnswerBtn.attr("id", "t" + i);
         trueAnswerBtn.text("True");
+
         // False button
         var falseAnswerBtn = $("<button>");
         falseAnswerBtn.css("margin", "0px 15px 0px 15px");
-        falseAnswerBtn.attr("class", "button is-medium answer");
+        falseAnswerBtn.attr("class", "button is-medium answer not-selected");
         falseAnswerBtn.attr("id", "f" + i);
         falseAnswerBtn.text("False");
+
         // Add correct id to the right button
         if (correctAnswer === "True") {
           trueAnswerBtn.attr("value", "correct");
         } else {
           falseAnswerBtn.attr("value", "correct");
         }
+
         // add buttons to btnDiv
         btnDiv.append(trueAnswerBtn);
         btnDiv.append(falseAnswerBtn);
+
         // Append btnDiv to the page on emptyDiv
         gameDiv.prepend(questionDiv, btnDiv);
       }
@@ -92,9 +93,38 @@ $(".game-div").on("click", ".answer", function() {
     buttonId = buttonId.replace("f", "t");
   }
   $("#" + buttonId).removeClass("is-info");
+  $("#" + buttonId).addClass("not-selected");
+  $(this).removeClass("not-selected");
   $(this).addClass("is-info");
 });
 
-$(".submit").on("click", ".game", function() {
+$(".submit").on("click", function() {
+  $(".submit").remove();
+  var btns = $(".is-info");
+  var notSelected = $(".not-selected");
+  console.log(notSelected);
+  for (var i = 0; i < btns.length; i++) {
+    console.log(btns[i]);
+    if (btns[i].value === "correct") {
+      correct++;
+      btns[i].disabled = true;
+      notSelected[i].remove();
+    } else {
+      btns[i].remove();
+      notSelected[i].disabled = true;
+    }
+  }
   console.log(correct);
+  $(".score").html(
+    "<strong>You got " + correct + " out of 5 correct!</strong>"
+  );
+  // Display the play again button, and set correct to 0/
+  $(".again").show();
+  correct = 0;
+});
+
+// button listener to play again. This will refresh the page
+$(".again").on("click", function() {
+  // refresh page
+  window.location.reload();
 });
